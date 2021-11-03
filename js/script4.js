3989440c3154d84554de8eb15b55b687eab94233
Yurii-Shutkin <itorik@ukr.net>
2021-11-03 07:31:20 +0200

    Update script4.js

--- js/script4.js
+++ js/script4.js
@@ -1,7 +1,4 @@
 const numArr = [1, -3, 5, 6, -7, 8, 9, -11];
-const stringsArr = ['aaa', 'aaaqqq', 'zzzqq', 'zz', 'qsaa', 'q', 'az'];
-const subArr = [1, 2, [3, 4], 5, [6, 7]];
-const someArr = [5, -3, 6, -5, 0, -7, 8, 9];
 
 // Task 16
 const positiveNumberArr = numArr.filter(item => item > 0);
@@ -12,13 +9,19 @@ const evenNumberArr = numArr.filter(item => item % 2 === 0);
 console.log(evenNumberArr);
 
 // Task 18
+const stringsArr = ['aaa', 'aaaqqq', 'zzzqq', 'zz', 'qsaa', 'q', 'az'];
+
 const lengthMoreFive = stringsArr.filter(item => item.length > 5);
 console.log(lengthMoreFive);
 
 // Task 19
+const subArr = [1, 2, [3, 4], 5, [6, 7]];
+
 const arrays = subArr.filter(item => Array.isArray(item));
 console.log(arrays);
 
 // Task 20
+const someArr = [5, -3, 6, -5, 0, -7, 8, 9];
+
 const negativeNumArr = someArr.filter(item => item < 0);
 console.log(negativeNumArr.length);
